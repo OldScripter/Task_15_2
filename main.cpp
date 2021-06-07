c2d4feb44909de96f84f7e1a76ed3dee30fd41f1
OldScripter <Oldscripter@gmail.com>
2021-06-07 22:45:05 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+void FindPair(std::vector<int> v, const int result)
+{
+    for (int i = 0; i < v.size() - 1; i ++)
+    {
+        for (int j = i + 1; j < v.size(); j++)
+        {
+            if (v[i] + v[j] == result)
+            {
+                std::cout << v[i] << " + " << v[j] << " = " << result << "\n";
+                std::cout << "Indexes: " << i << " and " << j << "\n";
+                return;
+            }
+        }
+    }
+    std::cout << "Appropriate pair is not found.\n";
+    return;
+}
+
+
+
+int main() {
+
+    std::vector<int> input = {2, 7, 11, 15};
+    int result = 1;
+
+    FindPair(input, result);
+    return 0;
+}
